01da97dfc579b8984602194102193574fbfab8a0
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2026-01-31 10:12:55 +0700

    tambah audit dao

--- app/src/main/java/com/example/remedialucp2_077/room/AuditLog.kt
+++ app/src/main/java/com/example/remedialucp2_077/room/AuditLog.kt
@@ -1,3 +1,14 @@
 package com.example.remedialucp2_077.room
 
-data class AuditLog()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class AuditLog(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val tableName: String,
+    val beforeData: String,
+    val afterData: String,
+    val timestamp: Long = System.currentTimeMillis()
+)
