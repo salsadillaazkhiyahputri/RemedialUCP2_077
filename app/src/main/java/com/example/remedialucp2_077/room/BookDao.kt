162f0af346d1448d10cbca7fd7c24bef02627d68
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2026-01-31 10:14:44 +0700

    tambah book dao

--- app/src/main/java/com/example/remedialucp2_077/room/BookDao.kt
+++ app/src/main/java/com/example/remedialucp2_077/room/BookDao.kt
@@ -1,4 +1,18 @@
 package com.example.remedialucp2_077.room
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface BookDao {
+
+    @Insert
+    suspend fun insert(book: Book)
+
+    @Query("SELECT * FROM Book WHERE isDeleted = 0")
+    suspend fun getAll(): List<Book>
+
+    @Query("UPDATE Book SET isDeleted = 1 WHERE id = :id")
+    suspend fun softDelete(id: Long)
 }
