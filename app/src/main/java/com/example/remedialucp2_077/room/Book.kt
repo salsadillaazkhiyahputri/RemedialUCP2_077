a97fbb8dd7b7c1c9336495beaea0bcfa6ab30a63
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2026-01-31 10:13:43 +0700

    tambah entity book

--- app/src/main/java/com/example/remedialucp2_077/room/Book.kt
+++ app/src/main/java/com/example/remedialucp2_077/room/Book.kt
@@ -1,3 +1,12 @@
 package com.example.remedialucp2_077.room
 
-data class Book()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Book(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val title: String,
+    val isDeleted: Boolean = false
+)
