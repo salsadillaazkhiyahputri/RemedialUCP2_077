413e3082ade918aaf15ddd8c1dda5b49698282d4
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2026-01-31 10:13:35 +0700

    tambah entity author

--- app/src/main/java/com/example/remedialucp2_077/room/Author.kt
+++ app/src/main/java/com/example/remedialucp2_077/room/Author.kt
@@ -1,3 +1,12 @@
 package com.example.remedialucp2_077.room
 
-data class Author()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class Author(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String
+)
