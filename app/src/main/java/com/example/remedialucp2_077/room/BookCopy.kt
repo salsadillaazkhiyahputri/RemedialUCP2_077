59dec24fe417a90d9968161ed298ef548258eabb
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2026-01-31 10:14:33 +0700

    tambah entity book copy

--- app/src/main/java/com/example/remedialucp2_077/room/BookCopy.kt
+++ app/src/main/java/com/example/remedialucp2_077/room/BookCopy.kt
@@ -1,3 +1,22 @@
 package com.example.remedialucp2_077.room
 
-data class BookCopy()
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = Book::class,
+            parentColumns = ["id"],
+            childColumns = ["bookId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class BookCopy(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val bookId: Long,
+    val status: String
+)
