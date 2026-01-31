eebdcb868800f257460817e2b2158bf6d0ffa385
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2026-01-31 10:13:10 +0700

    tambah audit dao

--- app/src/main/java/com/example/remedialucp2_077/room/AuditDao.kt
+++ app/src/main/java/com/example/remedialucp2_077/room/AuditDao.kt
@@ -1,4 +1,10 @@
 package com.example.remedialucp2_077.room
 
+import androidx.room.Dao
+import androidx.room.Insert
+
+@Dao
 interface AuditDao {
+    @Insert
+    suspend fun insert(log: AuditLog)
 }
