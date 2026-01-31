aecf08513905f6801ed5c283eef9d50964033384
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2026-01-31 10:14:58 +0700

    tambah entity category

--- app/src/main/java/com/example/remedialucp2_077/room/Category.kt
+++ app/src/main/java/com/example/remedialucp2_077/room/Category.kt
@@ -1,3 +1,13 @@
 package com.example.remedialucp2_077.room
 
-data class Category()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Category(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val parentId: Long? = null,
+    val isDeleted: Boolean = false
+)
